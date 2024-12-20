408e10fa650c5e2ff5b636ae2521f5b622dd1539
Enes Yesil <me.enesyesil@gmail.com>
2024-12-20 03:14:54 -0500

    edit json

--- my-commerce-backend/api/types/expres.d.ts
+++
@@ -1,9 +0,0 @@
-import { JwtPayload } from 'jsonwebtoken';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: string | JwtPayload; // Adjust the type based on your token payload
-    }
-  }
-}
