4a8b639fbae38e944cc6578080e122f71218b2d0
Enes Yesil <me.enesyesil@gmail.com>
2024-12-19 23:23:09 -0500

    dir changed

--- my-commerce-backend/api/index.js
+++ my-commerce-backend/api/index.js
@@ -1,4 +1,4 @@
-import app from '../dist/src/app'; // Correct path to compiled app.js
+import app from '../dist/app'; // Correct path to compiled app.js
 
 export default (req, res) => {
   app(req, res); // Export Express app as a serverless function
