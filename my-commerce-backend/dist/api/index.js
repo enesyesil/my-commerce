126b3cbf9c3da6b0e318dd28af29693eecbfacb6
Enes Yesil <me.enesyesil@gmail.com>
2024-12-19 23:11:59 -0500

    update index

--- my-commerce-backend/dist/api/index.js
+++ my-commerce-backend/dist/api/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../dist/app"));
+const app_1 = __importDefault(require("../dist/src/app"));
 exports.default = (req, res) => {
     (0, app_1.default)(req, res);
 };
