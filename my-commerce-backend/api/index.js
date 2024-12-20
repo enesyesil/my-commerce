a289de8dc50f7b091af9abeb2eadf0a7bf6cb3d4
Enes Yesil <me.enesyesil@gmail.com>
2024-12-20 02:08:18 -0500

    port changed

--- my-commerce-backend/api/index.js
+++ my-commerce-backend/api/index.js
@@ -25,5 +25,5 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
